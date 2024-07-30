eec5b5a57c396e3fbb55366195cbe609d9da00d5
yongsik.jang <yongsik.jang@wesang.com>
2024-07-30 14:15:30 +0900

    test action 커밋

--- dist/index.js
+++ dist/index.js
@@ -34472,9 +34472,19 @@ const github = __nccwpck_require__(5438);
 async function run() {
   try {
     const context = github.context;
+
     const ref = context.ref; // This will be in the format "refs/heads/branch-name"
     const branchName = ref.replace('refs/heads/', '');
+    const commitSha = context.sha;
+    const payload = context.payload;
+    const workflow = context.workflow;
+    const eventName= context.eventName;
+    const job= context.job;
+    const runId= context.runId;
+    const runNumber= context.runNumber;
+
     const exampleInput = core.getInput('exampleInput');
+
     var newVersion = 'default';
 
     if (/^feature\/.+$/.test(branchName) || /^dev.+$/.test(branchName)) {
@@ -34483,7 +34493,7 @@ async function run() {
       newVersion = 'from-tag'
     }
 
-    core.setOutput('branch-name', ref);
+    core.setOutput('branch-name', branchName);
     core.setOutput('new-version', newVersion);
     core.setOutput('example-input', exampleInput);
 
@@ -34491,6 +34501,13 @@ async function run() {
     console.log(`Branch Name: ${branchName}`);
     console.log(`Example Input: ${exampleInput}`);
 
+    console.log(`commitSha: ${commitSha}`);
+    console.log(`payload: ${payload}`);
+    console.log(`workflow: ${workflow}`);
+    console.log(`eventName: ${eventName}`);
+    console.log(`job: ${job}`);
+    console.log(`runId: ${runId}`);
+    console.log(`runNumber: ${runNumber}`);
   } catch (error) {
     core.setFailed(error);
   }
